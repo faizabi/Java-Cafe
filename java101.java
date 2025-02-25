6cee77df6de6661e1b6098d042aaaaf8f3af464e
faizabi <email2zabi@gmail.com>
2025-02-25 10:08:15 +0530

    file-01

---
+++ java101.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Data{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+         int k= sc.nextInt();
+        // int b= sc.nextInt();
+        char c = sc.next().charAt(0);
+
+        
+        System.out.println(k+c);
+        // System.out.println(c);
+    }
+}
